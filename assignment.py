e26242243f8c859c92e1f4860f735df2efa7497e
prash38 <pramireddy@ctemplar.com>
2020-11-21 12:29:50 -0500

    "stencil" code for model class, test, train, and preprocess

--- assignment.py
+++ assignment.py
@@ -1,1 +1,47 @@
+import os
+import numpy as np
+import tensorflow as tf
+from preprocess import *
+import sys
 
+
+class Model(tf.keras.Model):
+    def __init__(self):
+        """
+        The Model class predicts the wOBA value of a given hitting/pitching/fielding setup combination.
+        """
+        super(Model, self).__init__()
+
+
+    def call(self, inputs):
+        """
+        Runs model layers and embedding calculations that will be used to update gradients in train() and test()
+        :param inputs:
+        :return: probs:
+        """
+        pass
+
+    def loss(self, probs, labels):
+        """
+        Calculates average loss of the prediction
+        :param probs:
+        :param labels:
+        :return: the loss of the model as a tensor of size 1
+        """
+        pass
+
+
+def train(model, training_data):
+    pass
+
+
+def test(model, testing_data):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,11 @@
+import numpy as np
+import tensorflow as tf
 
+def get_data(data_file):
+    """
+    Takes in csv file and reads it to gather training and testing data and labels.
+    :param data_file: file path of the csv data
+    :return: Tuple of data containing:
+    (
+    """
+    pass
